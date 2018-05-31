e8cdadddb992c5dfd54a5bd5a69c66be020f8f3a
Rahon <39620650+Rahon1@users.noreply.github.com>
2018-05-31 20:08:56 +0200

    adssa

--- scripts/main.js
+++ scripts/main.js
@@ -18,11 +18,11 @@ var backgroundResize1 = function(){
     element.style.height = element.offsetWidth * 0.14;
 }
 
-var backgroundResize2 = function() {
+var backgroundResize2 = function(){
     element.style.height = element.offsetWidth * 0.50;
 }
 
-var backgroundResize3 = function() {
+var backgroundResize3 = function(){
     element.style.height = element.offsetWidth * 2;
 }
 
@@ -69,72 +69,3 @@ function refreshResize(){
         window.addEventListener("resize", backgroundResize1);
     }
 }
-
-
-/* Spielplatz */ /* Bitte keine Beachtung schenken */
-
-// var mql = window.matchMedia("(max-width:960px)");
-
-// mql.addListener(pictureResolutinChange);
-
-// function pictureResolutinChange()
-// {
-//     if(mql.matches)
-//     {
-//         document.querySelector("#slider img").src = "bilder/berg1[1500x350].jpeg";
-//     }
-//     else
-//     {
-//         document.querySelector("#slider img").src ="bilder/berg1[1500x300].jpeg";
-//     }
-// }
-
-/*  Mouseover function den image element übergeben damit die Pfeile nur angezeigt werden wenn die Mause über dem Bild ist.
-    Abschnitt 1 funktioniert, 2 kennt er element x nicht.
-
-console.log("hi");
-
-
-console.log("hi1");
-
-
-var hiddenArrowButton = document.querySelectorAll("#slider img")
-
-for(var i = 0; i < hiddenArrowButton.length; i++)
-{
-    hiddenArrowButton[i].addEventListener("mouseover", function()
-    {
-        console.log("hi2");
-
-        var x = document.querySelectorAll(".arrowButton");
-        for(var i = 0; i < 2; i++)
-        {   
-            if(x[i].className != "arrowButton hidden")
-                {
-                    x[i].className += " hidden";
-                }
-        
-        }
-    });
-   
-}
-
-// for(var i = 0; i < hiddenArrowButton.length; i++)
-// {
-//     hiddenArrowButton[i].addEventListener("mouseout", function()
-//     {
-//         console.log("hi3");
-
-//         var x = document.querySelectorAll(".arrowButton");
-
-//         console.log("hi4");
-//         for(var i = 0; i < 2; i++)
-//         {
-//             console.log("hi5");
-//             x.className -= " hidden";
-//         }
-//     });
-   
-// }
-
-*/
